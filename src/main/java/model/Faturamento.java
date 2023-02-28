1e4364789ab23322b2e647164a91c2ae4a7830dd
Guilherme Wanderley Espinola <gwanderleyespinola@gmail.com>
2023-02-28 13:40:00 -0300

    fix: getters e setters em Faturamento

--- src/main/java/model/Faturamento.java
+++ src/main/java/model/Faturamento.java
@@ -10,22 +10,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Faturamento {
+
     private String dia;
+
     private Double valor;
-
-    public String getDia() {
-        return dia;
-    }
-
-    public void setDia(String dia) {
-        this.dia = dia;
-    }
-
-    public Double getValor() {
-        return valor;
-    }
-
-    public void setValor(Double valor) {
-        this.valor = valor;
-    }
 }
